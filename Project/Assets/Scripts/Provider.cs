8dda2c9177ae20c9c0a73f409c39c0f56b3dddaf
Alexander Bronner <acb8570@g.rit.edu>
2015-10-25 19:09:00 -0400

    Added basic module providers.

---
+++ Project/Assets/Scripts/Provider.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Provider : NetworkBehaviour
+{
+    public GameObject visualPrefab;
+    public string objectName;
+
+    // Use this for initialization
+    void Start()
+    {
+        GameObject visual = (GameObject)Instantiate(
+            visualPrefab,
+            this.transform.position,
+            this.transform.rotation);
+        visual.transform.parent = this.transform;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        //Substitute action.
+        //other.
+    }
+}
