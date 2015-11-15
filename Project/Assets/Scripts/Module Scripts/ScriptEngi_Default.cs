94e4b5ed1be3107b0455463f213b86d91ad8257c
Alexander Bronner <acb8570@g.rit.edu>
2015-11-15 10:29:37 -0500

    Replaced Engi_Manuver with Engi_Slide, further enhanced default maneuverability.

--- Project/Assets/Scripts/Module Scripts/ScriptEngi_Default.cs
+++ Project/Assets/Scripts/Module Scripts/ScriptEngi_Default.cs
@@ -9,11 +9,14 @@ public class ScriptEngi_Default : MonoBehaviour
     public float maxRotateY;                    //maximum Y axis rotation
     public int sensetivityRotateX;              //X axis rotation sensetivity
     public int sensetivityRotateY;              //Y axis rotation sensetivity
+    public int autoRollRate;                    //Rate of auto-roll
     public int maxSpeed;                        //Maximum speed
     public int accelRate;                       //Acceleration strength
     public float deaccelRate;                   //Active deacceleration magnitude
     public float angleDrag;                     //Angular drag
     public float moveDrag;                      //Movement drag
+    public float lerpMultiplier;                //Multiplier for velocity lerping.
+    public float bounceMultiplier;              //Multiplier for bounce effect.
 
     private float lerpRate;                     //Scale (0-1) of lerping rigidbody velocity to forward
 
@@ -27,6 +30,16 @@ public class ScriptEngi_Default : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        moveYaw();
+        movePitch();
+        moveRoll();
+        moveThrust();
+        moveManipulate();
+    }
+
+    //Left-Right movement
+    void moveYaw()
+    {
         //Mouse x-axis yaw
         float rotate = Input.GetAxisRaw("Mouse X") * sensetivityRotateX;
         if (rotate > maxRotateX)    //Limit rotation
@@ -38,9 +51,13 @@ public class ScriptEngi_Default : MonoBehaviour
             rotate = -maxRotateX;
         }
         rb.AddRelativeTorque(Vector3.up * rotate * Time.deltaTime);
+    }
 
+    //Up-Down movement
+    void movePitch()
+    {
         //Mouse y-axis pitch
-        rotate = Input.GetAxisRaw("Mouse Y") * sensetivityRotateY;
+        float rotate = Input.GetAxisRaw("Mouse Y") * sensetivityRotateY;
         if (rotate > maxRotateY)    //Limit rotation
         {
             rotate = maxRotateY;
@@ -50,7 +67,11 @@ public class ScriptEngi_Default : MonoBehaviour
             rotate = -maxRotateY;
         }
         rb.AddRelativeTorque(Vector3.right * rotate * Time.deltaTime);
+    }
 
+    //Roll movement
+    void moveRoll()
+    {
         /*
         //Mouse click roll
         if (Input.GetMouseButton(0))
@@ -64,18 +85,21 @@ public class ScriptEngi_Default : MonoBehaviour
         */
 
         float temp = transform.parent.transform.rotation.eulerAngles.z;
-        print(temp);
 
         //Auto roll
         if (temp > 180 && temp < 360)
         {
-            rb.AddRelativeTorque(Vector3.forward * Time.deltaTime * 240 * (360 - temp) / 180);
+            rb.AddRelativeTorque(Vector3.forward * Time.deltaTime * autoRollRate * (360 - temp) / 180);
         }
         if (temp > 0 && temp < 180)
         {
-            rb.AddRelativeTorque(-Vector3.forward * Time.deltaTime * 240 * temp / 180);
+            rb.AddRelativeTorque(-Vector3.forward * Time.deltaTime * autoRollRate * temp / 180);
         }
+    }
 
+    //Forward-back movement
+    void moveThrust()
+    {
         //Key input for accelerate and deaccelerate
         if (Input.GetKey("w") && rb.velocity.magnitude < maxSpeed)
         {
@@ -85,23 +109,34 @@ public class ScriptEngi_Default : MonoBehaviour
         {
             rb.AddForce(-rb.velocity * deaccelRate);
         }
+    }
 
+    //Misc movement stuff. Drag, lerp, etc.
+    void moveManipulate()
+    {
         //Drag
         rb.AddTorque(-rb.angularVelocity * angleDrag * Time.deltaTime);
         rb.AddForce(-rb.velocity * moveDrag * Time.deltaTime);
 
         //Increase lerprate back to 1f
-        lerpRate += .4f * Time.deltaTime;
+        lerpRate += .1f * Time.deltaTime;
         if (lerpRate > 1)
             lerpRate = 1f;
 
         //Lerp velocity to forward direction
-        rb.velocity = Vector3.Lerp(rb.velocity, transform.forward * rb.velocity.magnitude, .5f * lerpRate);
+        rb.velocity = Vector3.Lerp(rb.velocity, transform.forward * rb.velocity.magnitude, lerpMultiplier * lerpRate);
+
+        //Limit velocity to max speed after a single bounce. Prevents chaining bounces to get ludicrous speed.
+        if(rb.velocity.magnitude > maxSpeed * bounceMultiplier)
+        {
+            rb.velocity = rb.velocity.normalized * maxSpeed * bounceMultiplier;
+        }
     }
 
     //Sets lerping to zero to cause collision disorientation.
     public void Disorient()
     {
         lerpRate = 0;
+        rb.velocity *= bounceMultiplier;
     }
 }
