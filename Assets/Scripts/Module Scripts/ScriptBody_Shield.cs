846e656e52a091dbb1a64af1610ef7c911c5fa58
Laura Durant <lkd8070@rit.edu>
2015-12-03 14:46:15 -0500

    Most of shield code

--- Assets/Scripts/Module Scripts/ScriptBody_Shield.cs
+++ Assets/Scripts/Module Scripts/ScriptBody_Shield.cs
@@ -10,4 +10,42 @@ public class ScriptBody_Shield : ScriptBody_Default
 	private float shielding = 0;
 
 	//link shield activation to Q
+	/*
+	void KeyPressed()
+	{
+		//if Q clicked
+		shielding = duration;
+		timeLeft = cooldown;
+	}
+	*/
+	
+	void Update()
+	{
+		if( isLocalPlayerDerived )
+		{
+			shielding -= Time.deltaTime;
+			timeLeft -= cooldown;
+			if( timeLeft <= 0 )
+			{
+				//make ability active
+			}
+		}
+	}
+	
+	public override void LoseHealth(Component bulletScript, float damage)
+    {
+        if (isLocalPlayerDerived && shielding <= 0)
+        {
+            health -= damage;
+            print("lost health!");
+            if (health <= 0)
+            {
+                //do something
+                parentCore.Spawn();
+                health = STARTING_HEALTH;
+            }
+            NetworkServer.Destroy(bulletScript.gameObject);
+			parentCore.TransmitHealth(health);
+        }
+    }
 }
