23e65b749798de5845f4b360a8ee70cac4e2e1e2
Alex Bronner <acb8570@g.rit.edu>
2015-12-04 12:51:24 -0500

    Fixed bounce module getting slightly crazy.

--- Assets/Scripts/Module Scripts/ScriptEngi_Manuver.cs
+++ Assets/Scripts/Module Scripts/ScriptEngi_Manuver.cs
@@ -3,6 +3,29 @@ using System.Collections;
 
 public class ScriptEngi_Manuver : ScriptEngi_Default
 {
+    public float bounceCut;
+    public float bounceRaise;
+    public float minBounceMultiplier;
+    private float maxBounceMultiplier;
 
+    public override void subStart()
+    {
+        maxBounceMultiplier = bounceMultiplier;
+    }
 
+    public override void subMoveManipulate()
+    {
+        bounceMultiplier += bounceRaise * Time.deltaTime;
+
+        if (bounceMultiplier > maxBounceMultiplier)
+            bounceMultiplier = maxBounceMultiplier;
+    }
+
+    public override void subDisorient()
+    {
+        bounceMultiplier *= bounceCut;
+
+        if (bounceMultiplier < minBounceMultiplier)
+            bounceMultiplier = minBounceMultiplier;
+    }
 }
